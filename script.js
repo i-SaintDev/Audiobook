2718aaec591815f109b116c3dd8459e37c88db31
Israel Santana <issantos11@hotmail.com>
2025-10-15 23:01:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const nomeCapitulo = document.getElementById("capitulo");
+const audio = document.getElementById("audio-capitulo");
+const botaoPlayPause = document.getElementById("play-pause");
+const botaoProximoCapitulo = document.getElementById("proximo");
+const botaoCapituloAnterior = document.getElementById("anterior");
+
+const quantidadeCapitulos = 10;
+let taTocando = false;
+let capitulo = 1;
+
+function tocarFaixa() {
+    audio.play();
+    taTocando = true;
+    botaoPlayPause.classList.add("tocando");
+}
+
+function pausarFaixa() {
+    audio.pause();
+    taTocando = false;
+    botaoPlayPause.classList.remove("tocando");
+}
+
+function tocarOuPausarFaixa() {
+    if (taTocando === true) {
+        pausarFaixa();
+    } else {
+        tocarFaixa();
+    }
+}
+
+function capituloAnterior() {
+    pausarFaixa();
+
+    if (capitulo === 1) {
+        capitulo = quantidadeCapitulos;
+    } else {
+        capitulo -= 1;
+    }
+
+    audio.src = "./audios/" + capitulo + ".mp3";
+    nomeCapitulo.innerText = "Capítulo " + capitulo;
+    tocarFaixa();
+}
+
+function proximoCapitulo() {
+    pausarFaixa();
+
+    if (capitulo < quantidadeCapitulos) {
+        capitulo += 1;
+    } else {
+        capitulo = 1;
+    }
+
+    audio.src = "./audios/" + capitulo + ".mp3";
+    nomeCapitulo.innerText = "Capítulo " + capitulo;
+    tocarFaixa();
+}
+
+botaoPlayPause.addEventListener("click", tocarOuPausarFaixa);
+botaoCapituloAnterior.addEventListener("click", capituloAnterior);
+botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
+
+audio.addEventListener("ended", proximoCapitulo);
+audio.addEventListener("timeupdate", () => {
+    const progresso = (audio.currentTime / audio.duration) * 100;
+    document.getElementById("barra-progresso").style.width = `${progresso}%`;
+});
